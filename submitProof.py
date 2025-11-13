59221219977002ef75219d35e252523330e7b481
serafina007 <zeyuqi@seas.upenn.edu>
2025-11-12 19:51:41 -0500

    Update submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -160,7 +160,7 @@ def send_signed_msg(proof, random_leaf):
 
     # TODO YOUR CODE HERE
     contract = w3.eth.contract(address=address, abi=abi)
-    tx_hash = contract.functions.submit(proof, random_leaf).build_transaction({
+    tx = contract.functions.submit(proof, random_leaf).build_transaction({
         'from': acct.address,
         'nonce': w3.eth.get_transaction_count(acct.address),
         'gas': 500000,
